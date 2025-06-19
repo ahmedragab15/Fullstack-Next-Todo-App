6f2fef856caa7c964f2a2acf33f1599007fce764
ahmedragab15 <ragabahmed154@gmail.com>
2025-06-19 09:15:08 +0300

    todo-dialog

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,52 @@
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>
-        <Plus /> New Todo
-      </Button>
+      <Dialog>
+        <form>
+          <DialogTrigger asChild>
+            <Button>
+              <Plus /> New Todo
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Add a new todo</DialogTitle>
+              <DialogDescription>Make changes to your profile here. Click save when you&apos;re done.</DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4">
+              <div className="grid gap-3">
+                <Label htmlFor="name-1">Name</Label>
+                <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="username-1">Username</Label>
+                <Input id="username-1" name="username" defaultValue="@peduarte" />
+              </div>
+            </div>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="outline">Cancel</Button>
+              </DialogClose>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </DialogContent>
+        </form>
+      </Dialog>
     </main>
   );
 }
