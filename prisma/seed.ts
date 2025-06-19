a503dba38fa9e305a0e4ed06d6626dea993426c4
ahmedragab15 <ragabahmed154@gmail.com>
2025-06-19 14:26:31 +0300

    prisma & mongoDB setup

---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client";
+import { faker } from "@faker-js/faker";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.createMany({
+    data: Array.from({ length: 25 }, () => ({
+      email: faker.internet.email(),
+      name: faker.person.fullName(),
+      address: {
+        city: faker.location.city(),
+        street: faker.location.street(),
+        state: faker.location.state(),
+        zip: faker.location.zipCode(),
+      },
+    })),
+  });
+}
+
+main()
+  .catch(async (e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
