6492b0ab2df1453a80f5b2551466f6a0a1658d1c
ahmedragab15 <ragabahmed154@gmail.com>
2025-06-21 12:28:02 +0300

    edit readme file

--- actions/todoActions.ts
+++ actions/todoActions.ts
@@ -5,48 +5,64 @@ import { revalidatePath } from "next/cache";
 const prisma = new PrismaClient();
 
 export const getUserTodoListAction = async ({ userId }: { userId: string | null }) => {
-  const userTodos = await prisma.todo.findMany({
-    where: {
-      userId: userId || "",
-    },
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
-  return userTodos;
+  try {
+    const userTodos = await prisma.todo.findMany({
+      where: {
+        userId: userId || "",
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    return userTodos;
+  } catch (error) {
+    throw new Error("Something went wrong" + error);
+  }
 };
 
 export const createTodoAction = async ({ title, body, completed, userId }: ITodo) => {
-  await prisma.todo.create({
-    data: {
-      title,
-      body,
-      completed,
-      userId: userId || "",
-    },
-  });
-  revalidatePath("/");
+  try {
+    await prisma.todo.create({
+      data: {
+        title,
+        body,
+        completed,
+        userId: userId || "",
+      },
+    });
+    revalidatePath("/");
+  } catch (error) {
+    throw new Error("Something went wrong" + error);
+  }
 };
 
 export const updateTodoAction = async ({ id, title, body, completed }: ITodo) => {
-  await prisma.todo.update({
-    where: {
-      id,
-    },
-    data: {
-      title,
-      body,
-      completed,
-    },
-  });
-  revalidatePath("/");
+  try {
+    await prisma.todo.update({
+      where: {
+        id,
+      },
+      data: {
+        title,
+        body,
+        completed,
+      },
+    });
+    revalidatePath("/");
+  } catch (error) {
+    throw new Error("Something went wrong" + error);
+  }
 };
 
 export const deleteTodoAction = async ({ id }: { id: string | undefined }) => {
-  await prisma.todo.delete({
-    where: {
-      id,
-    },
-  });
-  revalidatePath("/");
+  try {
+    await prisma.todo.delete({
+      where: {
+        id,
+      },
+    });
+    revalidatePath("/");
+  } catch (error) {
+    throw new Error("Something went wrong" + error);
+  }
 };
--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,13 @@ import { auth } from "@clerk/nextjs/server";
 export default async function Home() {
   const { userId } = await auth();
   const todos = await getUserTodoListAction({ userId });
-  console.log(userId);
 
   return (
     <main className="flex min-h-screen flex-col gap-2 p-24">
       <div className="self-end">
         <AddTodoForm userId={userId} />
       </div>
-        <TodosTable todos={todos} />
+      <TodosTable todos={todos} />
     </main>
   );
 }
